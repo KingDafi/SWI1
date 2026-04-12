f1eaa34528c829b3435f756b383a330ea4bea0d5
KingDafi <77487349+KingDafi@users.noreply.github.com>
2026-04-12 12:09:26 +0200

    Initial commit

---
+++ src/main/java/cz/osu/swi1_sm/Swi1SmApplication.java
@@ -0,0 +1,13 @@
+package cz.osu.swi1_sm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swi1SmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swi1SmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/osu/swi1_sm/Swi1SmApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.osu.swi1_sm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swi1SmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
