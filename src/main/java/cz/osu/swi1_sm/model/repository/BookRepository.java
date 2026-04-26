1695c4c3bb00ef710f8c671f77616e9c3a0ef76a
KingDafi <77487349+KingDafi@users.noreply.github.com>
2026-04-26 16:48:08 +0200

    Added BookRepository

---
+++ src/main/java/cz/osu/swi1_sm/model/repository/BookRepository.java
@@ -0,0 +1,15 @@
+package cz.osu.swi1_sm.model.repository;
+
+import cz.osu.swi1_sm.model.entity.Book;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends CrudRepository<Book, UUID> {
+    List<Book> findByTitleContainingIgnoreCase(String title);
+    List<Book> findByAuthorContainingIgnoreCase(String author);
+    List<Book> findByCategoryContainingIgnoreCase(String category);
+}
