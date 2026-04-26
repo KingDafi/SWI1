c81e6d7482dd024f70e0e38df7fbc1f7f2d08240
KingDafi <77487349+KingDafi@users.noreply.github.com>
2026-04-26 16:45:04 +0200

    Added AppUserRepository

---
+++ src/main/java/cz/osu/swi1_sm/model/repository/AppUserRepository.java
@@ -0,0 +1,13 @@
+package cz.osu.swi1_sm.model.repository;
+
+import cz.osu.swi1_sm.model.entity.AppUser;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AppUserRepository extends CrudRepository<AppUser, UUID> {
+    boolean existsByUsernameIgnoreCase(String username);
+    AppUser findByUsernameIgnoreCase(String username);
+}
