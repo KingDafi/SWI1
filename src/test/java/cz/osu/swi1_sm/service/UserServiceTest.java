251d51c4039d5bdac7b9ef71885183684be449e1
KingDafi <77487349+KingDafi@users.noreply.github.com>
2026-04-27 11:27:53 +0200

    Added tests for UserService

---
+++ src/test/java/cz/osu/swi1_sm/service/UserServiceTest.java
@@ -0,0 +1,56 @@
+package cz.osu.swi1_sm.service;
+
+import cz.osu.swi1_sm.model.dto.UserToken;
+import cz.osu.swi1_sm.model.entity.AppUser;
+import cz.osu.swi1_sm.model.entity.Role;
+import cz.osu.swi1_sm.model.repository.AppUserRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class UserServiceTest {
+    @Mock
+    private AppUserRepository userRepository;
+
+    @InjectMocks
+    private UserService userService;
+
+    @Test
+    void login_validCredentials_returnsUserToken() {
+        AppUser user = new AppUser();
+        user.setUsername("student@osu.cz");
+        user.setPassword("heslo");
+        user.setRole(Role.MEMBER);
+
+        when(userRepository.findByUsernameIgnoreCase("student@osu.cz")).thenReturn(user);
+
+        UserToken token = userService.login("student@osu.cz", "heslo");
+
+        assertEquals("student@osu.cz", token.getName());
+        assertEquals(Role.MEMBER, token.getRole());
+    }
+
+    @Test
+    void login_wrongPassword_throwsException() {
+        AppUser user = new AppUser();
+        user.setUsername("student@osu.cz");
+        user.setPassword("heslo");
+
+        when(userRepository.findByUsernameIgnoreCase("student@osu.cz")).thenReturn(user);
+
+        assertThrows(RuntimeException.class, () -> userService.login("student@osu.cz", "wrongpassword"));
+    }
+
+    @Test
+    void login_userDoesNotExist_throwsException() {
+        when(userRepository.findByUsernameIgnoreCase("unknown@osu.cz")).thenReturn(null);
+
+        assertThrows(IllegalArgumentException.class, () -> userService.login("unknown@osu.cz", "heslo"));
+    }
+}
